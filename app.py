95f228d1b26139c020aa8edf03ad4d2d985191d4
damian dicks <damiandicks54@gmail.com>
2026-03-21 09:20:03 +0200

    modified README.md and added new file

---
+++ app.py
@@ -0,0 +1,10 @@
+def say_thanks(User_Name, User_Surname, Feedback, Feedback_Rating):
+    input(User_Name)
+    input(User_Surname)
+    print("Thanks for sparing the time to enter your name")
+    input(Feedback)
+    input(Feedback_Rating)
+    print("Thanks we appreciate your feedback")
+
+say_thanks("enter your first name : ", "enter your surname : ", "How was your service today? : ", "How good was your service on a scale from 1 - 10 : " )
+
