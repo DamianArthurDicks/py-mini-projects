bd20d37841efb6efa388bf3a519f2f801c95b55c
damian dicks <damiandicks54@gmail.com>
2026-03-21 14:04:10 +0200

    updated project

--- Calculator_Project.py
+++ Calculator_Project.py
@@ -1,40 +1,14 @@
-"""def cube(num):
-    return num*num*num
+num1 = float(input("Enter first number : "))
+op = input("Enter operator : ")
+num2 = float(input("Enter second number : "))
 
-result = cube(5)
-print(result)"""
-
-"""is_drive_far = False
-is_radio_working = False
-
-if is_drive_far or is_radio_working:
-    print("the drive is far")
-    print("the radio is working")
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "/":
+    print(num1 / num2)
+elif op == "*":
+    print(num1 * num2)
 else:
-    print("the drive is not far")
-    print("the radio isn't working")"""
-
-"""is_drive_far = False
-is_radio_working = False
-
-if is_drive_far and is_radio_working:
-    print("the drive is far")
-    print("the radio is working")
-elif is_drive_far and not(is_radio_working):
-    print("drive is far and radio isn't working")
-elif not(is_drive_far) and is_radio_working:
-    print("drive isn't far and radio is working")
-else:
-    print("the drive is not far")
-    print("the radio isn't working")"""
-
-def max_num(num1, num2, num3):
-    if num1 >= num2 and num1 >= num3:
-        return num1
-    elif num2 >= num1 and num2 >= num3:
-        return num2
-    else:
-        return num3
-    
-
-print(max_num(5, 6, 7))
+    print("invalid operator")
