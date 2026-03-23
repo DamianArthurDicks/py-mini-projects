fe477f5bae38f903e1b9f9e0a930df63b947bc2b
damian dicks <damiandicks54@gmail.com>
2026-03-23 15:05:09 +0200

    new file

---
+++ Nested_Looping_Thru_2D_List.py
@@ -0,0 +1,10 @@
+Number_Grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [0]
+]
+
+for row in Number_Grid:  #this nested loop, loops thru the row and the column in each row
+    for col in row:
+        print(col) 
