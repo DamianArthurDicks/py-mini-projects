3482a22634b703be4f4893684de74beaf5cbb7b2
damian dicks <damiandicks54@gmail.com>
2026-03-26 15:19:26 +0200

    modified and new files

--- Hangman_Game.py
+++ Hangman_Game.py
@@ -4,11 +4,11 @@ Guess_Count = 0
 Guess_Limit = 12
 Out_of_Guesses = False
 
-while Guess != Hangman_Letters and not (Out_of_Guesses):
+while Guess != Hangman_Letters and not (Out_of_Guesses):   #while that loops until out of guesses
     if Guess_Count < Guess_Limit:
-        Guess.append(input("Guess a letter: ").upper())
-        Guess_Count += 1
-        print("Guess Again")
+        Guess.append(input("Guess a letter: ").upper())   #if letter is guessed in lower case it is coverted in to uppercase
+        Guess_Count += 1       #every guess counted is added to the guess list                
+        print("Guess Again")   #guess count adds guess number until it has reached its guess limit
     else:
         Out_of_Guesses = True
 if Out_of_Guesses:
---
+++ Try_and_Except.py
@@ -0,0 +1,5 @@
+try:
+   Number = int(input("enter a number: "))
+   print(Number)
+except ValueError:
+   print("Invalid Data Type")
