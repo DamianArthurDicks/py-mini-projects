d1c5b33cdf165377fe9574f649c5b0679f3afa60
damian dicks <damiandicks54@gmail.com>
2026-04-11 18:21:41 +0200

    new files

---
+++ Useful_Tools_App.py
@@ -0,0 +1,3 @@
+import Useful_Tools_Module
+
+print(Useful_Tools_Module.roll_dice(10))
---
+++ Useful_Tools_Module.py
@@ -0,0 +1,11 @@
+import random
+
+Feet_in_Mile = 5280
+Meters_in_Kilometer = 1000
+Beatles = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Star"]
+
+def get_file_ext(filename):
+    return filename[filename.index(".") + 1: ]
+
+def roll_dice(num):
+    return random.randint(1, num)
