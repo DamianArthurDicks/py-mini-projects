ef57f1de54508035234adf5edec807cf84b14ae1
damian dicks <damiandicks54@gmail.com>
2026-04-12 12:42:35 +0200

    new files and modified files

---
+++ Chef_Class.py
@@ -0,0 +1,11 @@
+class Chef:
+
+    def Make_Chicken(self):
+        print("The Chef makes a Chicken")
+
+    def Make_Salad(self):
+        print("The Chef makes a Salad")
+
+    def Make_Special_Dish(self):
+        print("The Chef makes a BBQ Ribs")
+    
--- Student_Class.py
+++ Student_Class.py
@@ -1,7 +1,13 @@
-class Student:
+class Student:     #Class defines the object data type
 
-    def __init__(self, name, major, gpa, is_on_probation):  #Class defines the object data type
-        self.name = name                                    
-        self.major = major
-        self.gpa = gpa
-        self.is_on_probation = is_on_probation
+    def __init__(self, name, major, gpa, is_on_probation):  #these parenthesis passes info to the object
+        self.name = name     #this make sure that the object name == name that we passed in                               
+        self.major = major   #this make sure that the object major == major that we passed in
+        self.gpa = gpa       #this make sure that the object gpa == gpa that we passed in
+        self.is_on_probation = is_on_probation  #same for is on probation
+
+    def on_honor_roll(self):   #this is a class function that checks if the student is on honor roll
+        if  self.gpa >= 3.5:
+            return True
+        else:
+            return False
